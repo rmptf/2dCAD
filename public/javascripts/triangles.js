8cd7469bed4d0383ff1b1f30d48d893e10f7fcea
rmptf <rmpotthoff@example.com>
2022-12-02 10:32:08 -0500

    taking apart formula

--- public/javascripts/triangles.js
+++ public/javascripts/triangles.js
@@ -25,6 +25,7 @@ function findSlope(x1, y1, x2, y2)
 
 path2.pointA.x = 300
 path2.pointA.y = 10
+// (blue point)
 
 // This draws a perpendicular line from some point on path1 to a set point
 if (path1.pointA.y == path1.pointB.y) { // AB is horizontal
@@ -35,6 +36,7 @@ if (path1.pointA.y == path1.pointB.y) { // AB is horizontal
     path2.pointB.y = path2.pointA.y
     } else { // need some geometry
     var gradientOfpath1 = (path1.pointA.y - path1.pointB.y) / (path1.pointA.x - path1.pointB.x);
+    
     var interceptOfpath1 = path1.pointA.y - gradientOfpath1 * path1.pointA.x;
     
     var gradientOfpath2 = -1 / gradientOfpath1;
@@ -42,6 +44,43 @@ if (path1.pointA.y == path1.pointB.y) { // AB is horizontal
     
     path2.pointB.x = (interceptOfpath1 - interceptOfpath2) / (gradientOfpath2 - gradientOfpath1);
     path2.pointB.y = gradientOfpath2 * path2.pointB.x + interceptOfpath2;
+    // (green point)
+
+    // console.log('p2Bx = ')
+    // console.log(path2.pointB.x)
+
+    // console.log(interceptOfpath1)
+    // console.log(interceptOfpath2)
+    // console.log(gradientOfpath1)
+    // console.log(gradientOfpath2)
+
+
+    // Path1:
+    // slope1 = (a1Y - b1Y) / (a1X - b1X)
+    // intercept1 = a1Y - slope1 * a1X
+
+    // Path2:
+    // slope2 = -1 / slope
+    // intercept2 = a2Y - slope2 * a2X
+
+    // b2X = (intercept1 - intercept2) / (slope2 - slope1) // x = (b1 - b2) / (m1 - m2)
+    // // point slope formula??: (y – y1) = m(x – x1) 
+    // b2Y = slope2 * b2X + intercept2 // y = mx + b
+    // // slope intercept formular: y = mx + b
+
+    // path3:
+    // set (b3X, b3Y) to (Path1 midpoint X, Path1 midpoint Y)
+    // slope3 = -1 / slope
+    // intercept3 = b3Y - slope3 * b3X
+
+    // a3X = set length along slope3
+    // a3Y = set length along slope3
+
+
+    
+    //     y = mx + b
+
+    // y = (slope3 * x) + yIntercept    
 
     // path2.pointB.x = 0;
     // path2.pointB.y = -interceptOfpath2 / gradientOfpath2;
