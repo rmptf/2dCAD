bb6edbb878b2dad823d3947b48fed2ba6fbe9990
rmptf <rmpotthoff@example.com>
2023-03-29 07:49:44 -0400

    color themeing

--- public/reference_files/module_locations.js
+++ public/reference_files/module_locations.js
@@ -3,6 +3,8 @@ const MODULE_LOCATIONS = {
 
     A_WINDOW: '../../../../../../../../views/partials/modules/html/second_level_modules/windows/a-window/a-window_files/a-window.ejs',
     A_WINDOW_CENTERED: '../../../../../../../../views/partials/modules/html/second_level_modules/windows/a-window/a-window--centered_files/a-window--centered.ejs',
+
+    A_TESTER: '../../../../../../../../views/partials/modules/html/third_level_modules/testers/a-tester/a-tester_files/a-tester.ejs',
 }
 
 module.exports = { 
