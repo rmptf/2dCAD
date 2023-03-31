6af9d57a817f975b65697548e9f65f4222b57ac1
rmptf <rmpotthoff@example.com>
2023-03-31 08:16:12 -0400

    modules

--- public/reference_files/module_locations.js
+++ public/reference_files/module_locations.js
@@ -1,10 +1,25 @@
 const MODULE_LOCATIONS = {
+    // FIRST LEVEL
     A_PANE: '../views/partials/modules/html/first_level_modules/panes/a-pane/a-pane_files/a-pane.ejs',
+
+    // SECOND LEVEL
+    A_ARTBOARD: '../../../../../../../../views/partials/modules/html/second_level_modules/artboards/a-artboard/a-artboard_files/a-artboard.ejs',
 
     A_WINDOW: '../../../../../../../../views/partials/modules/html/second_level_modules/windows/a-window/a-window_files/a-window.ejs',
     A_WINDOW_CENTERED: '../../../../../../../../views/partials/modules/html/second_level_modules/windows/a-window/a-window--centered_files/a-window--centered.ejs',
 
+    // THIRD LEVEL
+    A_SVGCANVAS: '../../../../../../../../views/partials/modules/html/third_level_modules/svg_canvases/a-svg_canvas/a-svg_canvas_files/a-svg_canvas.ejs',
+
+    A_HEADER: '',
+    B_HEADER: '../../../../../../../../views/partials/modules/html/third_level_modules/headers/b-header/b-header_files/b-header.ejs',
+
+    A_FOOTER: '',
+    B_FOOTER: '../../../../../../../../views/partials/modules/html/third_level_modules/footers/b-footer/b-footer_files/b-footer.ejs',
+
     A_TESTER: '../../../../../../../../views/partials/modules/html/third_level_modules/testers/a-tester/a-tester_files/a-tester.ejs',
+
+    
 }
 
 module.exports = { 
