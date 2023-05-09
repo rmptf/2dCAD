809427e69a3d2cb761a9104e7d3fb7b506437075
rmptf <rmpotthoff@example.com>
2023-05-09 08:11:05 -0400

    testing github

--- public/javascripts/curvePoints.js
+++ public/javascripts/curvePoints.js
@@ -263,6 +263,8 @@ function drawLine() {
             let βDeg = βRad * (180/Math.PI)
 
             console.log(αDeg, βDeg)
+
+            // adding something to git
         }
         
         // ----------------------------------------------------------------//
