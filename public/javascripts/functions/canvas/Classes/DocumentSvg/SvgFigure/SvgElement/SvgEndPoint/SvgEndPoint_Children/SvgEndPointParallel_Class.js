e818e17de79093f5f9dfcdbf176d6443d5452d56
Ryan <rmpotthoff@gmail.com>
2024-07-15 17:07:38 -0400

    added joiner_ class

--- public/javascripts/functions/canvas/Classes/DocumentSvg/SvgFigure/SvgElement/SvgEndPoint/SvgEndPoint_Children/SvgEndPointParallel_Class.js
+++ public/javascripts/functions/canvas/Classes/DocumentSvg/SvgFigure/SvgElement/SvgEndPoint/SvgEndPoint_Children/SvgEndPointParallel_Class.js
@@ -3,6 +3,7 @@ import {SvgEndPoint} from '../SvgEndPoint_Class.js'
 function SvgEndPointParallel(parentFigure, parentElement, pathData, index) {
     this.ENDPOINT_CLASS = 'parallelEndPoint'
     this.ENDPOINT_CURVE_CLASS = 'paralellEndPoint_curve'
+    this.ENDPOINT_JOINER_CLASS = 'joiner_'
     SvgEndPoint.call(this, parentFigure, parentElement, pathData, index)
 }
 
@@ -13,6 +14,10 @@ SvgEndPointParallel.prototype.createSvgEndPoint = function(index) {
     let newEndPointParallel = SvgEndPoint.prototype.createSvgEndPoint.call(this, index)
         .on("click", (event) => this.elementClick(event, this.actionStates))
     newEndPointParallel.node().classList.add(this.ENDPOINT_CLASS)
+    if(this.pathData.arc.joiner === true) {
+        console.log("polplplplpllpldd")
+        newEndPointParallel.node().classList.add(this.ENDPOINT_JOINER_CLASS)
+    }
     if(this.pathData.arc.exist === true && this.pathData.arc.side === 'east') {
         newEndPointParallel.node().classList.add(this.ENDPOINT_CURVE_CLASS)
     }
