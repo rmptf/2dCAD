9d4ac4b4503c32dbd1d1c46553f00464f46742c7
Ryan <rmpotthoff@gmail.com>
2024-09-04 17:02:28 -0400

    working on model

--- routes/utils/ejsModels/ejsModelBuilder/artBoardmodel/artBoardModelObjects.js
+++ routes/utils/ejsModels/ejsModelBuilder/artBoardmodel/artBoardModelObjects.js
@@ -6,12 +6,35 @@ const {COLORVARS, WIDTHVARS, BTNVARS} = require('../../../../../public/reference
 const MODEL_OBJECT_01 = 'B_PANE'
 const MODEL_OBJECT_02 = 'A_ARTBOARD'
 const MODEL_OBJECT_03 = 'B_HEADER'
+const MODEL_OBJECT_04 = 'A_CANVAS'
+const MODEL_OBJECT_05 = 'A_DOCUMENT'
+const MODEL_OBJECT_06 = 'B_FOOTER'
 
 const MODEL_OBJECTS = {
     MODEL_GLOBAL_VARIABLES: {
         GLOBAL_MODE: global.globalModeClass,
         MODULE_THEME: COLORVARS.THEMECLASS.defaultThemeClass,
     },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     MODEL_OBJECT_01: {
         MODULE_NAME: MODEL_OBJECT_01,
         MODULE_LOCATION: MODULE_LOCATIONS.B_PANE,
@@ -59,7 +82,22 @@ const MODEL_OBJECTS = {
             ],
         }
     },
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -110,6 +148,22 @@ const MODEL_OBJECTS = {
             ],
         }
     },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -174,7 +228,7 @@ const MODEL_OBJECTS = {
                                     btnTheme_status: BTNVARS.BTNSTATUS.disabled,
                                     btnTheme_style: BTNVARS.BTNSTYLE.contained,
                                 },
-                            }, // TODO: RIGHT HERE
+                            },
                         ]
                     }
                 },
@@ -189,16 +243,391 @@ const MODEL_OBJECTS = {
             ],
         }
     },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    MODEL_OBJECT_04: {
+        MODULE_NAME: MODEL_OBJECT_04,
+        MODULE_LOCATION: MODULE_LOCATIONS.A_CANVAS,
+        MODULE_THEMES: {
+            THEME_ContainerFill: COLORVARS.FILLS.mainFill,
+        },
+        MODULE_CONTENT: {
+            CONTENT_module_content: null,
+        },
+        MODULE_DATA: {
+            DATA_module_data: null,
+        },
+        MODULE_ACTIONS: {
+            BUTTONS: [
+                {
+                    BUTTON: {
+                        BTN_DATA: null,
+                    },
+                },
+            ],
+            BUTTON_CONTAINERS: [
+                {
+                    CONTAINER: {
+                        CONTAINER_DATA: {
+                            DATA: null,
+                        },
+                        BUTTONS: [
+                            {
+                                BUTTON: {
+                                    BTN_DATA: null,
+                                },
+                            },
+                        ]
+                    }
+                },
+            ],
+        },
+        MODULE_CHILDREN_DATA: {
+            CHILDREN_NAMES: [
+                null
+            ],
+            CHILDREN: [
+                null
+            ],
+        }
+    },
+
+
+
+
+
+
+    MODEL_OBJECT_05: {
+        MODULE_NAME: MODEL_OBJECT_05,
+        MODULE_LOCATION: MODULE_LOCATIONS.A_DOCUMENT,
+        MODULE_THEMES: {
+            THEME_containerFill: COLORVARS.FILLS.mainFill,
+            THEME_containerColor: COLORVARS.COLORS.main,
+            THEME_containerBorderColor: COLORVARS.BORDERS.mainMutedBorderColor,
+            THEME_svgFill: COLORVARS.FILLS.mainMuted,
+        },
+        MODULE_CONTENT: {
+            CONTENT_module_content: null,
+        },
+        MODULE_DATA: {
+            DATA_module_data: null,
+        },
+        MODULE_ACTIONS: {
+            BUTTONS: [
+                {
+                    BUTTON: {
+                        BTN_DATA: null,
+                    },
+                },
+            ],
+            BUTTON_CONTAINERS: [
+                {
+                    CONTAINER: {
+                        CONTAINER_DATA: {
+                            headerText: "",
+                            btnDirection: "btn-container--row",
+                            btnAlign: "btn-align--center",
+                            },
+                        BUTTONS: [
+                            {
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "Path",
+                                    btnId: "aDoc_btnCont01_btn01",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                            },
+                            {
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "EndPoint",
+                                    btnId: "aDoc_btnCont01_btn069",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                            },
+                            {
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "EndPoint_Curve",
+                                    btnId: "aDoc_btnCont01_btn02",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                            },
+                            {
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "Parallel",
+                                    btnId: "aDoc_btnCont01_btn03",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                            },
+                            {
+                                BUTTON: {
+                                    btn_04: {
+                                        btnExist: true,
+                                        btnType: "button",
+                                        btnText: "Measure",
+                                        btnId: "aDoc_btnCont01_btn04",
+                                        btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                        btnTheme_type: BTNVARS.BTNTYPES.default,
+                                        btnTheme_size: BTNVARS.BTNSIZES.small,
+                                        btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                        btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                    },
+                                },
+                            },
+                        ]
+                    },
+                },
+                {
+                    CONTAINER: {
+                        CONTAINER_DATA: {
+                            headerText: "DRAW FIGURE",
+                            btnDirection: "btn-container--row",
+                            btnAlign: "btn-align--center",
+                        },
+                        BUTTONS: [
+                            {
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "SaveFigure",
+                                    btnId: "aDoc_btnCont02_btn01",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "SaveSvg",
+                                    btnId: "aDoc_btnCont02_btn069",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "DeleteEndPoint",
+                                    btnId: "aDoc_btnCont02_btn02",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "DrawSavedFigure",
+                                    btnId: "aDoc_btnCont02_btn03",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "DrawSavedSvg",
+                                    btnId: "aDoc_btnCont02_btn04",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                            },
+                        ]
+                    },
+                },
+            ],
+        },
+        MODULE_CHILDREN_DATA: {
+            CHILDREN_NAMES: [
+                null
+            ],
+            CHILDREN: [
+                null
+            ],
+        }
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    MODEL_OBJECT_06: {
+        MODULE_NAME: MODEL_OBJECT_06,
+        MODULE_LOCATION: MODULE_LOCATIONS.B_FOOTER,
+        MODULE_THEMES: {
+            THEME_containerFill: COLORVARS.FILLS.mainMuted,
+        },
+        MODULE_CONTENT: {
+            CONTENT_module_content: null,
+        },
+        MODULE_DATA: {
+            DATA_module_data: null,
+        },
+        MODULE_ACTIONS: {
+            BUTTONS: [
+                {
+                    BUTTON: {
+                        BTN_DATA: null,
+                    },
+                },
+            ],
+            BUTTON_CONTAINERS: [
+                {
+                    CONTAINER: {
+                        CONTAINER_DATA: {
+                            btnDirection: "btn-container--row",
+                            btnAlign: "btn-align--left",
+                        },
+                        BUTTONS: [
+                            {
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "About",
+                                    btnOnClick: "window.location.href='/'",
+                                    btnId: "bFooter01_actons_btn01",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "New Svg",
+                                    btnOnClick: "",
+                                    btnId: "bFooter01_actons_btn02",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "+",
+                                    btnOnClick: "",
+                                    btnId: "bFooter01_actons_btn03",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "100%", 
+                                    btnOnClick: "",
+                                    btnId: "bFooter01_actons_btn04",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                                BUTTON: {
+                                    btnExist: true,
+                                    btnType: "button",
+                                    btnText: "-",
+                                    btnOnClick: "",
+                                    btnId: "bFooter01_actons_btn05",
+                                    btnTheme_class: BTNVARS.BTNCLASS.aButton,
+                                    btnTheme_type: BTNVARS.BTNTYPES.default,
+                                    btnTheme_size: BTNVARS.BTNSIZES.small,
+                                    btnTheme_status: BTNVARS.BTNSTATUS.disabled,
+                                    btnTheme_style: BTNVARS.BTNSTYLE.contained,
+                                },
+                            },
+                        ]
+                    }
+                },
+            ],
+        },
+        MODULE_CHILDREN_DATA: {
+            CHILDREN_NAMES: [
+                null
+            ],
+            CHILDREN: [
+                null
+            ],
+        }
+    },
+
+
+
+
+
 }
-
-
-
-
-
-// export {
-//     OBJECT_1,
-//     OBJECT_2
-// }
 
 module.exports = { 
     MODEL_OBJECTS
