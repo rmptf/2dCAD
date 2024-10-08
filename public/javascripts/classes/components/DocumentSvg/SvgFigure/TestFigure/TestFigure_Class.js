8df4ee37176e53b5e74fb3c4b51bdfecca88dcc2
rmptf <rmpotthoff@example.com>
2024-10-08 08:50:26 -0400

    woring on calculation  vis figures

--- public/javascripts/classes/components/DocumentSvg/SvgFigure/TestFigure/TestFigure_Class.js
+++ public/javascripts/classes/components/DocumentSvg/SvgFigure/TestFigure/TestFigure_Class.js
@@ -1,11 +1,18 @@
+// import {describeComplexPath} from '../../DocumentSvg_functions/documentSvg_animations/animation_functions/svgElementCalculationsNEW.js'
+import {describeComplexPath} from '../../../../../functions/math/svgElementCalculations.js'
 // import {makeDeepCopy} from '../ParallelFigure/parallelFigure_functions/parallelPathFunctions_NEW.js'
-// import {makeDeepCopy} from '../../../../../functions/drafting/parallelPath/drawParallelPath_functions/parallelPathFunctions.js'
+import {makeDeepCopy} from '../../../../../functions/drafting/parallelPath/drawParallelPath_functions/parallelPathFunctions.js'
 import {SvgGroup} from '../SvgElement/SvgGroup/SvgGroup_Class.js'
 
 function TestFigure(svgFigure, parFigure, id, doRun) {
     // updateSVG_highlight_2_points_1_line_02_B_NEW
     this.doRun = doRun
     this.testGroup = null
+
+    this.circleElements = []
+    this.lineElements = []
+    this.pathElements = []
+    this.rectElements = []
 
     if(this.doRun){
         this.id = id
@@ -14,12 +21,12 @@ function TestFigure(svgFigure, parFigure, id, doRun) {
         this.testFigureGroup = this.svgFigure.secondaryFigureGroups[4]
         this.testGroup = new SvgGroup(this.testFigureGroup, 'NEWTESTSUBGROUP_' + this.id, 'fakeId_group').newSvgGroup
         this.testRectGroup = new SvgGroup(this.testFigureGroup, 'NEWTESTSUBGROUP_' + this.id, 'fakeId_group').newSvgGroup
-        addRect(this.testRectGroup, this.testGroup)
+        addRect(this.testRectGroup, this.testGroup, this.rectElements)
     }
 }
 
-function addRect(rectGroup, figureGroup) {
-    rectGroup.append('rect')
+function addRect(rectGroup, figureGroup, rectElements) {
+    let rect = rectGroup.append('rect')
     .attr('class', 'none')
     .attr('id', 'testtest')
     .attr("stroke", "#FFFFFF1A")
@@ -43,35 +50,61 @@ function addRect(rectGroup, figureGroup) {
             figureGroup.style("visibility", "hidden")
         }
     })
+
+    rectElements.push(rect)
 }
 
-TestFigure.prototype.addCircle = function(elemId) {
+TestFigure.prototype.addCircle = function(elemId, visObj) {
     if(this.doRun) {
-        this.testGroup.append('circle')
-        .attr('class', 'testElement-endpoint testElement-palette--4 testElem-radius--10 testElem-fill-color--2')
+        let newCircle = this.testGroup.append('circle')
+        // .attr('class', 'testElement-endpoint testElement-palette--4 testElem-radius--10 testElem-fill-color--2')
+        .attr('class','testElement-endpoint testElement-palette--'+[visObj.palette]+' testElem-radius--'+[visObj.circRad]+' testElem-fill-color--'+[visObj.fillClr]+'')
         .attr('id', elemId + this.id)
+
+        this.circleElements.push(newCircle)
+        // let obj = {palette: 4, circRadius: 10, fillClr: 2}
     }
 }
 
-TestFigure.prototype.addPath = function(elemId) {
+TestFigure.prototype.addLine = function(elemId, visObj) {
     if(this.doRun) {
-        this.testGroup.append('line')
-        .attr('class', 'testElement-path testElement-palette--4 testElem-strokeWidth--1 testElem-stroke-color--2 testElem-dashArray--5')
+        let newPath = this.testGroup.append('line')
+        // .attr('class', 'testElement-path testElement-palette--4 testElem-strokeWidth--1 testElem-stroke-color--2 testElem-dashArray--5')
+        .attr('class', 'testElement-path testElement-palette--'+[visObj.palette]+' testElem-strokeWidth--'+[visObj.strkWdth]+' testElem-stroke-color--'+[visObj.strkClr]+' testElem-dashArray--'+[visObj.dshArray]+'')
         .attr('id', elemId + this.id)
+
+        this.lineElements.push(newPath)
+        // let obj = {palette: 4, strkWdth: 1, strkClr: 2, dshArray: 5}
+    }
+}
+
+TestFigure.prototype.addPath = function(elemId, visObj) {
+    if(this.doRun) {
+        let newPath = this.testGroup.append('path')
+        // .attr('class', 'testElement-path testElement-palette--4 testElem-strokeWidth--1 testElem-stroke-color--2 testElem-dashArray--5')
+        .attr('class', 'testElement-path testElement-palette--'+[visObj.palette]+' testElem-strokeWidth--'+[visObj.strkWdth]+' testElem-stroke-color--'+[visObj.strkClr]+' testElem-dashArray--'+[visObj.dshArray]+'')
+        .attr('id', elemId + this.id)
+
+        this.pathElements.push(newPath)
     }
 }
 
 TestFigure.prototype.updateTestFigure_111 = function(coords1, coords2) {
     if(this.doRun) {
         //FIXME: fix ids
-        let point1 = this.testGroup.select('#visualTest--intersectPt1--largeArcFlag-IDTAG_' + this.id)
-        let point2 = this.testGroup.select('#visualTest--intersectPt2--largeArcFlag-IDTAG_' + this.id)
-        let path = this.testGroup.select('#visualTest--path--largeArcFlag-IDTAG_' + this.id)
-        let rect =  this.testRectGroup.select('#testtest')
+        // let point1 = this.testGroup.select('#visualTest--intersectPt1--largeArcFlag-IDTAG_' + this.id)
+        // let point2 = this.testGroup.select('#visualTest--intersectPt2--largeArcFlag-IDTAG_' + this.id)
+        // let path = this.testGroup.select('#visualTest--path--largeArcFlag-IDTAG_' + this.id)
+        // let rect =  this.testRectGroup.select('#testtest')
+        let point1 = this.circleElements[0]
+        let point2 = this.circleElements[1]
+        let path = this.lineElements[0]
+        let rect = this.rectElements[0]
 
         point1.attr('cx', coords1[0]).attr('cy', coords1[1])
         point2.attr('cx', coords2[0]).attr('cy', coords2[1])
         path.attr('x1', coords1[0]).attr('y1', coords1[1]).attr('x2', coords2[0]).attr('y2', coords2[1])
+
         let bbox = this.testGroup.node().getBBox()
         rect.attr("x", bbox.x - 0).attr("y", bbox.y - 0).attr("width", bbox.width).attr("height", bbox.height)
     }
@@ -79,20 +112,6 @@ TestFigure.prototype.updateTestFigure_111 = function(coords1, coords2) {
 
 TestFigure.prototype.updateTestFigure_333 = function(pathData) {
     if(this.doRun) {
-        // //FIXME: fix ids
-        // let point1 = this.testGroup.select('#visualTest--intersectPt1--largeArcFlag-IDTAG_' + this.id)
-        // let point2 = this.testGroup.select('#visualTest--intersectPt2--largeArcFlag-IDTAG_' + this.id)
-        // let path = this.testGroup.select('#visualTest--path--largeArcFlag-IDTAG_' + this.id)
-        // let rect =  this.testRectGroup.select('#testtest')
-
-        // point1.attr('cx', coords1[0]).attr('cy', coords1[1])
-        // point2.attr('cx', coords2[0]).attr('cy', coords2[1])
-        // path.attr('x1', coords1[0]).attr('y1', coords1[1]).attr('x2', coords2[0]).attr('y2', coords2[1])
-        // let bbox = this.testGroup.node().getBBox()
-        // rect.attr("x", bbox.x - 0).attr("y", bbox.y - 0).attr("width", bbox.width).attr("height", bbox.height)
-
-
-        
         // GREEN
         let pathData1 = makeDeepCopy(pathData)
         pathData1[1].arc.arcFlag = 1
@@ -110,10 +129,15 @@ TestFigure.prototype.updateTestFigure_333 = function(pathData) {
         pathData4[1].arc.arcFlag = 0
         pathData4[1].arc.sweepFlag = 0
 
-        let path1 = this.testGroup.select("#intArcTEST--path1--largeArcFlag-IDTAG_" + this.id)
-        let path2 = this.testGroup.select("#intArcTEST--path2--largeArcFlag-IDTAG_" + this.id)
-        let path3 = this.testGroup.select("#intArcTEST--path3--largeArcFlag-IDTAG_" + this.id)
-        let path4 = this.testGroup.select("#intArcTEST--path4--largeArcFlag-IDTAG_" + this.id)
+        // let path1 = this.testGroup.select("#intArcTEST--path1--largeArcFlag-IDTAG_" + this.id)
+        // let path2 = this.testGroup.select("#intArcTEST--path2--largeArcFlag-IDTAG_" + this.id)
+        // let path3 = this.testGroup.select("#intArcTEST--path3--largeArcFlag-IDTAG_" + this.id)
+        // let path4 = this.testGroup.select("#intArcTEST--path4--largeArcFlag-IDTAG_" + this.id)
+        let path1 = this.pathElements[0]
+        let path2 = this.pathElements[1]
+        let path3 = this.pathElements[2]
+        let path4 = this.pathElements[3]
+        let rect = this.rectElements[0]
 
         path1.attr('d', describeComplexPath([pathData1[0], pathData1[1]]))
         path2.attr('d', describeComplexPath([pathData2[0], pathData2[1]]))
--- public/javascripts/functions/drafting/parallelPath/sortEndPoints/sortEndPoints_functions/setLargeArcFlag.js
+++ public/javascripts/functions/drafting/parallelPath/sortEndPoints/sortEndPoints_functions/setLargeArcFlag.js
@@ -100,7 +100,7 @@ function updateSVGArcFlags(index, prevTargetEndPoint, thisTargetEndPoint, midPoi
     // }
 
     if(index === 2) {
-        // updateSVG_highlight_1_path_3ways_arcFlag_sweepFlag_variations_02([prevTargetEndPoint, thisTargetEndPoint], self)
+        updateSVG_highlight_1_path_3ways_arcFlag_sweepFlag_variations_02([prevTargetEndPoint, thisTargetEndPoint], self)
         updateSVG_highlight_2_points_1_line_02_A([prevTargetEndPoint.coords.x, prevTargetEndPoint.coords.y], [thisTargetEndPoint.coords.x, thisTargetEndPoint.coords.y], self)
         updateSVG_highlight_2_points_1_line_02_B(midPointBetweenInts, [thisTargetEndPoint.arc.center.x, thisTargetEndPoint.arc.center.y], self)
     }
