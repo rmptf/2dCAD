f91323542f5a616db0c72579e041cb8dcbb845ff
rmptf <rmpotthoff@example.com>
2025-01-28 08:40:08 -0500

    small chnges

--- public/javascripts/classes/components/DocumentSvg/SvgFigure/ParallelFigure/ParallelFigure_Helper_Classes/LargeArcFlagSetter_Class.js
+++ public/javascripts/classes/components/DocumentSvg/SvgFigure/ParallelFigure/ParallelFigure_Helper_Classes/LargeArcFlagSetter_Class.js
@@ -9,26 +9,26 @@ function LargeArcFlagSetter(parallelFigure) {
     this.parallelFigureObj = parallelFigure.parallelFigureObject
     //old
     // this.index = null
-    this.counter = 0
+    this.iterationCounter = 0
     this.startSide = null
     let svgFigure = parallelFigure.svgFigure
 
-    // // Add ReferenceFigures
-    // this.referenceFigure_01 = new ReferenceFigure(svgFigure, true)
-    // this.referenceFigure_01.addCircle({palette: 4, circRad: 10, fillClr: 2}, 1)
-    // this.referenceFigure_01.addCircle({palette: 4, circRad: 10, fillClr: 2}, 2)
-    // this.referenceFigure_01.addLine({palette: 4, strkWdth: 1, strkClr: 2, dshArray: 5})
+    // Add ReferenceFigures
+    this.referenceFigure_01 = new ReferenceFigure(svgFigure, true)
+    this.referenceFigure_01.addCircle({palette: 4, circRad: 10, fillClr: 2}, 1)
+    this.referenceFigure_01.addCircle({palette: 4, circRad: 10, fillClr: 2}, 2)
+    this.referenceFigure_01.addLine({palette: 4, strkWdth: 1, strkClr: 2, dshArray: 5})
 
-    // this.referenceFigure_02 = new ReferenceFigure(svgFigure, true)
-    // this.referenceFigure_02.addCircle({palette: 4, circRad: 5, fillClr: 3}, 1)
-    // this.referenceFigure_02.addCircle({palette: 4, circRad: 5, fillClr: 3}, 2)
-    // this.referenceFigure_02.addLine({palette: 4, strkWdth: 1, strkClr: 3, dshArray: 5})
+    this.referenceFigure_02 = new ReferenceFigure(svgFigure, true)
+    this.referenceFigure_02.addCircle({palette: 4, circRad: 5, fillClr: 3}, 1)
+    this.referenceFigure_02.addCircle({palette: 4, circRad: 5, fillClr: 3}, 2)
+    this.referenceFigure_02.addLine({palette: 4, strkWdth: 1, strkClr: 3, dshArray: 5})
 
-    // this.referenceFigure_03 = new ReferenceFigure(svgFigure, false)
-    // this.referenceFigure_03.addPath({palette: 1, strkWdth: 1, strkClr: 3, dshArray: 2}, 1)
-    // this.referenceFigure_03.addPath({palette: 2, strkWdth: 2, strkClr: 2, dshArray: 5}, 2)
-    // this.referenceFigure_03.addPath({palette: 1, strkWdth: 1, strkClr: 3, dshArray: 5}, 3)
-    // this.referenceFigure_03.addPath({palette: 2, strkWdth: 5, strkClr: 2, dshArray: 'none'}, 4)
+    this.referenceFigure_03 = new ReferenceFigure(svgFigure, false)
+    this.referenceFigure_03.addPath({palette: 1, strkWdth: 1, strkClr: 3, dshArray: 2}, 1)
+    this.referenceFigure_03.addPath({palette: 2, strkWdth: 2, strkClr: 2, dshArray: 5}, 2)
+    this.referenceFigure_03.addPath({palette: 1, strkWdth: 1, strkClr: 3, dshArray: 5}, 3)
+    this.referenceFigure_03.addPath({palette: 2, strkWdth: 5, strkClr: 2, dshArray: 'none'}, 4)
 
 
     // this.referenceFigure_04 = new ReferenceFigure(svgFigure, false)
@@ -104,19 +104,20 @@ function LargeArcFlagSetter(parallelFigure) {
 
 LargeArcFlagSetter.prototype.setLargeArcFlag = function(indexModifier, runOrNot, index) {
     console.log("ARCFLAG_FLIPPER_RUNNING__________")
-    // let modifiedIndex = this.index + indexModifier
     let modifiedIndex = index + indexModifier
-
 
     let parallelEndPoint_start = this.originalFigurePathDatas[modifiedIndex].children.parallel_pathDatas.pathData_west
     let parallelEndPoint_end = this.originalFigurePathDatas[modifiedIndex + 1].children.parallel_pathDatas.pathData_east
     let midPointBetweenEndPoints = findLineMidpoint(parallelEndPoint_start.coords.x, parallelEndPoint_start.coords.y, parallelEndPoint_end.coords.x, parallelEndPoint_end.coords.y)
 
+    this.referenceFigure_01.runFunctions([[parallelEndPoint_start.coords.x, parallelEndPoint_start.coords.y], [parallelEndPoint_end.coords.x, parallelEndPoint_end.coords.y]])
+    this.referenceFigure_02.runFunctions([midPointBetweenEndPoints, [parallelEndPoint_end.arc.center.x, parallelEndPoint_end.arc.center.y]])
+    this.referenceFigure_03.runFunctions([[parallelEndPoint_start, parallelEndPoint_end]])
+
     if(runOrNot === true) {
+        // let crossedPoints = hasPathFlippedDirection()
         let crossedSides = setArcCenterAxisLine(parallelEndPoint_start, parallelEndPoint_end, midPointBetweenEndPoints, [parallelEndPoint_end.arc.center.x, parallelEndPoint_end.arc.center.y], this)
-        if(this.counter === 0) {
-            // console.log("START_SIDE_SET")
-            // console.log(crossedSides)
+        if(this.iterationCounter === 0) {
             this.startSide = crossedSides
         }
         
@@ -126,13 +127,12 @@ LargeArcFlagSetter.prototype.setLargeArcFlag = function(indexModifier, runOrNot,
         } 
     }
 
-
     function flipFlagAndFunction(targetEndPoint) {
         targetEndPoint.arc.arcFlag = +!targetEndPoint.arc.arcFlag
     }
 
-    if(this.counter < 10) {
-        this.counter = this.counter + 1
+    if(this.iterationCounter < 10) {
+        this.iterationCounter = this.iterationCounter + 1
     }
 }
 
@@ -157,33 +157,14 @@ function setArcCenterAxisLine(arcBaseStart, arcBaseFinish, arcBaseMidPoint, circ
     // New line's points by translating the original points
     // const arcCenterAxisStart = [px, py]
     // const arcCenterAxisFinish = [(px + dx), (py + dy)]
+    //TODO: Should i try to change the length of the line to have a minumul or be logner in general?
     const arcCenterAxisStart = [(px - (dx/2)), (py - (dy/2))]
     const arcCenterAxisFinish = [(px + (dx/2)), (py + (dy/2))]
 
-    // if(thisFigure.index === 0) {
-        thisFigure.referenceFigure_01_A.runFunctions([arcBaseMidPoint])
-        thisFigure.referenceFigure_02_A.runFunctions([circleCenter])
-        thisFigure.referenceFigure_03_A.runFunctions([arcCenterAxisStart, arcCenterAxisFinish])
-        thisFigure.referenceFigure_04_A.runFunctions([arcBaseMidPoint])
-    // }
-    // if(thisFigure.index === 1) {
-    //     thisFigure.referenceFigure_01_B.runFunctions([arcBaseMidPoint])
-    //     thisFigure.referenceFigure_02_B.runFunctions([circleCenter])
-    //     thisFigure.referenceFigure_03_B.runFunctions([arcCenterAxisStart, arcCenterAxisFinish])
-    //     thisFigure.referenceFigure_04_B.runFunctions([arcBaseMidPoint])
-    // }
-    // if(thisFigure.index === 2) {
-    //     thisFigure.referenceFigure_01_C.runFunctions([arcBaseMidPoint])
-    //     thisFigure.referenceFigure_02_C.runFunctions([circleCenter])
-    //     thisFigure.referenceFigure_03_C.runFunctions([arcCenterAxisStart, arcCenterAxisFinish])
-    //     thisFigure.referenceFigure_04_C.runFunctions([arcBaseMidPoint])
-    // }
-    // if(thisFigure.index === 3) {
-    //     thisFigure.referenceFigure_01_D.runFunctions([arcBaseMidPoint])
-    //     thisFigure.referenceFigure_02_D.runFunctions([circleCenter])
-    //     thisFigure.referenceFigure_03_D.runFunctions([arcCenterAxisStart, arcCenterAxisFinish])
-    //     thisFigure.referenceFigure_04_D.runFunctions([arcBaseMidPoint])
-    // }
+    thisFigure.referenceFigure_01_A.runFunctions([arcBaseMidPoint])
+    thisFigure.referenceFigure_02_A.runFunctions([circleCenter])
+    thisFigure.referenceFigure_03_A.runFunctions([arcCenterAxisStart, arcCenterAxisFinish])
+    thisFigure.referenceFigure_04_A.runFunctions([arcBaseMidPoint])
 
     return checkWhenPointHasCrossedAxis(arcCenterAxisStart, arcCenterAxisFinish, arcBaseMidPoint, thisFigure)
 }
@@ -202,33 +183,11 @@ function checkWhenPointHasCrossedAxis(axisStartCoords, axisFinishCoords, pointCo
 
          // Return the side based on the cross product
         if (crossProduct > 0) {
-            // if(thisFigure.index === 0) {
-                thisFigure.referenceFigure_04_A.changeCircleColor(4,1)
-            // }
-            // if(thisFigure.index === 1) {
-            //     thisFigure.referenceFigure_04_B.changeCircleColor(4,1)
-            // }
-            // if(thisFigure.index === 2) {
-            //     thisFigure.referenceFigure_04_C.changeCircleColor(4,1)
-            // }
-            // if(thisFigure.index === 3) {
-            //     thisFigure.referenceFigure_04_D.changeCircleColor(4,1)
-            // }
+            thisFigure.referenceFigure_04_A.changeCircleColor(4,1)
             console.log("NO_CROSSED_AXIS______")
             return true
         } else if (crossProduct < 0) {
-            // if(thisFigure.index === 0) {
-                thisFigure.referenceFigure_04_A.changeCircleColor(1,4)
-            // }
-            // if(thisFigure.index === 1) {
-            //     thisFigure.referenceFigure_04_B.changeCircleColor(1,4)
-            // }
-            // if(thisFigure.index === 2) {
-            //     thisFigure.referenceFigure_04_C.changeCircleColor(1,4)
-            // }
-            // if(thisFigure.index === 3) {
-            //     thisFigure.referenceFigure_04_D.changeCircleColor(1,4)
-            // }
+            thisFigure.referenceFigure_04_A.changeCircleColor(1,4)
             console.log("CROSS_AXIS______")
             console.log(thisFigure.index)
             return false
@@ -236,6 +195,123 @@ function checkWhenPointHasCrossedAxis(axisStartCoords, axisFinishCoords, pointCo
             // return "ONPATH_______.";
         }
 }
+
+function hasPathFlippedDirection(prevStart, prevEnd, currStart, currEnd) {
+    // Vectors for the two segments
+    const prevVectorX = prevEnd.x - prevStart.x;
+    const prevVectorY = prevEnd.y - prevStart.y;
+    const currVectorX = currEnd.x - currStart.x;
+    const currVectorY = currEnd.y - currStart.y;
+  
+    // Compute the cross product of the two vectors
+    const crossProduct = prevVectorX * currVectorY - prevVectorY * currVectorX;
+  
+    // Return the sign of the cross product
+    return crossProduct;
+  }
+  
+  // Helper function to detect if there's a sign flip
+  function detectSignFlip(crossProduct1, crossProduct2) {
+    return (crossProduct1 > 0 && crossProduct2 < 0) || (crossProduct1 < 0 && crossProduct2 > 0);
+  }
+
+
+// function setArcCenterAxisLine(arcBaseStart, arcBaseFinish, arcBaseMidPoint, circleCenter, thisFigure) {
+//     // Existing line coordinates
+//     const x1 = arcBaseStart.coords.x
+//     const y1 = arcBaseStart.coords.y
+//     const x2 = arcBaseFinish.coords.x
+//     const y2 = arcBaseFinish.coords.y
+
+//     // New point the parallel line should pass through
+//     const px = circleCenter[0], py = circleCenter[1]
+
+//     // Calculate the shift vector
+//     const dx = x2 - x1 // Difference in x
+//     const dy = y2 - y1 // Difference in y
+
+//     // New line's points by translating the original points
+//     // const arcCenterAxisStart = [px, py]
+//     // const arcCenterAxisFinish = [(px + dx), (py + dy)]
+//     const arcCenterAxisStart = [(px - (dx/2)), (py - (dy/2))]
+//     const arcCenterAxisFinish = [(px + (dx/2)), (py + (dy/2))]
+
+//     // if(thisFigure.index === 0) {
+//         thisFigure.referenceFigure_01_A.runFunctions([arcBaseMidPoint])
+//         thisFigure.referenceFigure_02_A.runFunctions([circleCenter])
+//         thisFigure.referenceFigure_03_A.runFunctions([arcCenterAxisStart, arcCenterAxisFinish])
+//         thisFigure.referenceFigure_04_A.runFunctions([arcBaseMidPoint])
+//     // }
+//     // if(thisFigure.index === 1) {
+//     //     thisFigure.referenceFigure_01_B.runFunctions([arcBaseMidPoint])
+//     //     thisFigure.referenceFigure_02_B.runFunctions([circleCenter])
+//     //     thisFigure.referenceFigure_03_B.runFunctions([arcCenterAxisStart, arcCenterAxisFinish])
+//     //     thisFigure.referenceFigure_04_B.runFunctions([arcBaseMidPoint])
+//     // }
+//     // if(thisFigure.index === 2) {
+//     //     thisFigure.referenceFigure_01_C.runFunctions([arcBaseMidPoint])
+//     //     thisFigure.referenceFigure_02_C.runFunctions([circleCenter])
+//     //     thisFigure.referenceFigure_03_C.runFunctions([arcCenterAxisStart, arcCenterAxisFinish])
+//     //     thisFigure.referenceFigure_04_C.runFunctions([arcBaseMidPoint])
+//     // }
+//     // if(thisFigure.index === 3) {
+//     //     thisFigure.referenceFigure_01_D.runFunctions([arcBaseMidPoint])
+//     //     thisFigure.referenceFigure_02_D.runFunctions([circleCenter])
+//     //     thisFigure.referenceFigure_03_D.runFunctions([arcCenterAxisStart, arcCenterAxisFinish])
+//     //     thisFigure.referenceFigure_04_D.runFunctions([arcBaseMidPoint])
+//     // }
+
+//     return checkWhenPointHasCrossedAxis(arcCenterAxisStart, arcCenterAxisFinish, arcBaseMidPoint, thisFigure)
+// }
+
+// function checkWhenPointHasCrossedAxis(axisStartCoords, axisFinishCoords, pointCoords, thisFigure) {
+//         // Vector from start to end of the path
+//         const pathVectorX = axisFinishCoords[0] - axisStartCoords[0]
+//         const pathVectorY = axisFinishCoords[1] - axisStartCoords[1]
+      
+//         // Vector from start of path to the point
+//         const pointVectorX = pointCoords[0] - axisStartCoords[0]
+//         const pointVectorY = pointCoords[1] - axisStartCoords[1]
+
+//          // Compute the cross product
+//          const crossProduct = pathVectorX * pointVectorY - pathVectorY * pointVectorX;
+
+//          // Return the side based on the cross product
+//         if (crossProduct > 0) {
+//             // if(thisFigure.index === 0) {
+//                 thisFigure.referenceFigure_04_A.changeCircleColor(4,1)
+//             // }
+//             // if(thisFigure.index === 1) {
+//             //     thisFigure.referenceFigure_04_B.changeCircleColor(4,1)
+//             // }
+//             // if(thisFigure.index === 2) {
+//             //     thisFigure.referenceFigure_04_C.changeCircleColor(4,1)
+//             // }
+//             // if(thisFigure.index === 3) {
+//             //     thisFigure.referenceFigure_04_D.changeCircleColor(4,1)
+//             // }
+//             console.log("NO_CROSSED_AXIS______")
+//             return true
+//         } else if (crossProduct < 0) {
+//             // if(thisFigure.index === 0) {
+//                 thisFigure.referenceFigure_04_A.changeCircleColor(1,4)
+//             // }
+//             // if(thisFigure.index === 1) {
+//             //     thisFigure.referenceFigure_04_B.changeCircleColor(1,4)
+//             // }
+//             // if(thisFigure.index === 2) {
+//             //     thisFigure.referenceFigure_04_C.changeCircleColor(1,4)
+//             // }
+//             // if(thisFigure.index === 3) {
+//             //     thisFigure.referenceFigure_04_D.changeCircleColor(1,4)
+//             // }
+//             console.log("CROSS_AXIS______")
+//             console.log(thisFigure.index)
+//             return false
+//         } else {
+//             // return "ONPATH_______.";
+//         }
+// }
 
 
 
