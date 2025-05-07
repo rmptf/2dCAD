bdefbf0b2ac8794fa7d3a4098bfd7365b1bbbeb4
rmptf <rmpotthoff@example.com>
2025-05-07 08:05:00 -0400

    just tinering no progress

--- public/javascripts/classes/components/DocumentSvg/DocumentSvg_functions/documentSvg_animations/updateDocumentSvg.js
+++ public/javascripts/classes/components/DocumentSvg/DocumentSvg_functions/documentSvg_animations/updateDocumentSvg.js
@@ -447,20 +447,30 @@ function updateSVG_thisSvgParallelFigure(figure, i) {
     let parallelPathDatas = figure.parallelFigurePathDatas //FIXME: old way need to update
     let parallelPaths = figure.svgPaths.parallelPaths //FIXME: old way, might need to update
 
-    let newIII = i - 1
-    let originalFigurePathDatas = figure.originalFigurePathDatas //FIXME: new way
-    // this.thisOriginalFigurePathData().children.parallel_pathDatas.pathData_west
-    // this.thisOriginalFigurePathData().children.parallel_pathDatas.pathData_east
+    // let newIII = i + 1
+    // let originalFigurePathDatas = figure.originalFigurePathDatas //FIXME: new way
+    // // this.thisOriginalFigurePathData().children.parallel_pathDatas.pathData_west
+    // // this.thisOriginalFigurePathData().children.parallel_pathDatas.pathData_east
 
     // //NOTOWKRING
     // // let prevPD = originalFigurePathDatas[newIII - 1].children.parallel_pathDatas.pathData_west
     // // let thisPD =  originalFigurePathDatas[newIII].children.parallel_pathDatas.pathData_east
 
-    // let prevPD = originalFigurePathDatas[newIII + 1].children.parallel_pathDatas.pathData_west
-    // let thisPD =  originalFigurePathDatas[newIII + 2].children.parallel_pathDatas.pathData_east
+    // // let prevPD = someValue !== undefined ? someValue : null;
+    // // let thisPD = someValue !== undefined ? someValue : null;
+    // // let myVar = typeof someValue !== 'undefined' ? someValue : null;
+
+
+    // // let prevPD = originalFigurePathDatas[newIII - 1].children.parallel_pathDatas.pathData_west
+    // // let thisPD =  originalFigurePathDatas[newIII].children.parallel_pathDatas.pathData_east
+
+    // let prevPD = originalFigurePathDatas[newIII - 1] !== undefined ? originalFigurePathDatas[newIII - 1].children.parallel_pathDatas.pathData_west : null;
+    // let thisPD = originalFigurePathDatas[newIII] !== undefined ? originalFigurePathDatas[newIII].children.parallel_pathDatas.pathData_east : null;
 
     // console.log("okokokokokokokok")
     // console.log("NEWWAY")
+    // // console.log(originalFigurePathDatas[newIII - 1])
+    // // console.log(originalFigurePathDatas[newIII])
     // console.log(prevPD)
     // console.log(thisPD)
     // console.log("OLDWAY")
@@ -474,7 +484,7 @@ function updateSVG_thisSvgParallelFigure(figure, i) {
         console.log(" ")
         console.log("RUNNING")
         console.log(i)
-        console.log(originalFigurePathDatas[i])
+        // console.log(originalFigurePathDatas[i])
         console.log(" ")
         parallelPaths[i].svgElementObject //FIXME: old way, might need to update
             .attr('d', describeComplexPath([parallelPathDatas[i][0], parallelPathDatas[i][1]])) //FIXME: old way, need to update AND might need to update describeComplexPath()
@@ -494,6 +504,35 @@ function updateSVG_thisSvgParallelFigure(figure, i) {
     }
     // PARALLEL END POINTS
 }
+
+
+// function updateSVG_thisSvgParallelFigure(figure) {
+//     let parallelPathDatas = figure.parallelFigurePathDatas
+//     let parallelPaths = figure.svgPaths.parallelPaths
+
+//     // PARALLEL PATH
+//     for (let i = 0; i < parallelPaths.length; i++) {
+//         console.log(" ")
+//         console.log("RUNNING")
+//         console.log(i)
+//         console.log(" ")
+//         parallelPaths[i].svgElementObject
+//             .attr('d', describeComplexPath([parallelPathDatas[i][0], parallelPathDatas[i][1]]))
+//     }
+//     // PARALLEL PATH
+
+//     // PARALLEL END POINTS
+//     let endPoints = figure.svgEndPoints
+//     let k = -1
+//     for (let i = 0; i < parallelPaths.length; i++) {
+//         for (let j = 0; j < parallelPathDatas[i].length; j++) {
+//             k = k + 1
+//             endPoints[k].svgElementObject
+//                 .attr('cx', parallelPathDatas[i][j].coords.x).attr('cy', parallelPathDatas[i][j].coords.y)
+//         }
+//     }
+//     // PARALLEL END POINTS
+// }
 
 
 export {
